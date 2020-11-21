60e2cc70ed279aef49916ae6070d3c70c690332b
Fart Butt <butts@fartcannon.com>
2020-11-21 16:02:54 -0500

    let's see if this monitor makes this crap pack worse

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 import datetime
 import logging
 from pathlib import Path
-from secrets import bot_key
+from secrets import bot_key, command_prefix, server_rcon_info
 import discord
+import asyncio
 from discord.ext.commands import Bot
 from cogs.minecraft_rebooter import MinecraftCrap
+from mcrcon import MCRcon
+import socket
+from library import do_send_message
+import subprocess
 
 intents = discord.Intents.default()
 intents.members = True
@@ -32,7 +37,7 @@ def setup_logger() -> logging.Logger:
 
 log = setup_logger()
 
-bot = Bot(description="a bot for minecraft", command_prefix="&", pm_help=False, intents=intents)
+bot = Bot(description="a bot for minecraft", command_prefix=command_prefix, pm_help=False, intents=intents)
 
 
 @bot.event
@@ -40,10 +45,69 @@ async def on_ready():
     log.info('Use this link to invite {}:'.format(bot.user.name))
     log.info('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(bot.user.id))
     log.info('--------')
-    log.info('You are running Rebootin Butt 0.0.1')
+    log.info('You are running Rebootin Butt 0.1.0')
     log.info('Created by Poop Poop')
     log.info('--------')
 
 
+def response_monitor(r):
+    if r > 20:
+        # 20 or more no responses from server.
+        print('its dead jim')
+        subprocess.run(
+            ["/home/taffer/minecraft/Valhelsia_SERVER-2.2.10/start.sh"]
+        )
+        await do_send_message(154337182717444096, "I'm rebooting this POS now")
+        # lock server in restart mode so monitor does not attempt to start a new instance
+        return 2
+    else:
+        return 0
+
+
+async def minecraft_server_monitor():
+    server_state = 0  # server states: 0= off, 1= on, 2= restarting
+    response_counter = 0
+    await bot.wait_until_ready()
+    log.debug("starting server monitor")
+    while not bot.is_closed():
+        await asyncio.sleep(10)
+        log.debug("running server monitor")
+        try:
+            with MCRcon(server_rcon_info['ip'], server_rcon_info['password']) as m:
+                resp = m.command("/list")
+                players = resp.split(": ")[1].split(", ")
+                log.debug("found online players: %s" % players)
+                if response_counter > 0:
+                    response_counter = 0  # reset counter
+                    await do_send_message(154337182717444096, "The server's back up, nerds")
+
+                server_state = 1
+        except TimeoutError:
+            if response_counter == 0:
+                await do_send_message(154337182717444096, "I think the server took a shit")
+            response_counter += 1
+            log.debug("server offline, counter is %d" % response_counter)
+            if not server_state == 2:
+                server_state = response_monitor(response_counter)
+
+        except socket.timeout:
+            if response_counter == 0:
+                await do_send_message(154337182717444096, "I think the server took a shit")
+            response_counter += 1
+            log.debug("server offline, counter is %d" % response_counter)
+            if not server_state == 2:
+                server_state = response_monitor(response_counter)
+
+        except ConnectionRefusedError:
+            if response_counter == 0:
+                await do_send_message(154337182717444096, "I think the server took a shit")
+            response_counter += 1
+            log.debug("server offline, counter is %d" % response_counter)
+            if not server_state == 2:
+                server_state = response_monitor(response_counter)
+
+
 bot.add_cog(MinecraftCrap(bot))
+socket.setdefaulttimeout(5)
+bot.loop.create_task(minecraft_server_monitor())
 bot.run(bot_key)
